fda08772b42f6893f7898ef5aaf8326aa7ecba2f
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-05-14 21:23:57 +0100

    implementation de la navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   App.js                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Barhamou <hamabarhamou@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 20:07:57 by Barhamou          #+#    #+#             */
+/*   Updated: 2022/05/14 21:19:11 by Barhamou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world !</h1>
-      <h1>begin of the trackegps work !</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path= "/about" element = {<About/>} />
+        <Route path= "*" element = {<Home/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <h2> begin the Tracker GPS of the engin </h2>
+        </div>
+    );
+};
+
+export default Home;
