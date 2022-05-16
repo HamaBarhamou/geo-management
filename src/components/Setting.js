c2f565aba087388618281c6d53b0ef9ca78d8d82
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-05-16 13:10:49 +0100

    ajout de deux input de type date

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -3,8 +3,13 @@ import React from 'react';
 const Setting = () => {
     return (
         <div>
+            <h1>Rapport</h1>
             <h2>Setting</h2>
-            <input type ="text"/>
+            <form>
+                <label> Depart :<input type="datetime-local" name="date_debut" /> </label>
+                <label> fin :<input type="datetime-local" name="date_fin" /> </label>
+                <input type="submit" value="Generer Rapport" />
+            </form>
         </div>
     );
 };
