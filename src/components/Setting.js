04fb4a2570662e338b4d3525545f7fc73a6bc7af
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-05-16 15:47:07 +0100

    rendu conditionnelle du paramettre de rapport

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const Setting = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        console.log(data)
+    };
     return (
         <div>
             <h1>Rapport</h1>
             <h2>Setting</h2>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("firstName")} />
+                <select {...register("gender")}>
+                    <option value="female">female</option>
+                    <option value="male">male</option>
+                    <option value="other">other</option>
+                </select>
+            <input type="submit" />
+            
                 <label> Depart :<input type="datetime-local" name="date_debut" /> </label>
                 <label> fin :<input type="datetime-local" name="date_fin" /> </label>
-                <input type="submit" value="Generer Rapport" />
+                
             </form>
         </div>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Body from '../components/Body';
 import Head from '../components/Head';
 import Onglet from '../components/Onglet'
 import Setting from '../components/Setting';
 import '../styles/App.scss';
+
 const Home = () => {
-    return (
-        <div>
-            <Head/>
-            <Onglet/>
-            <Setting/>
-            <Body/>
-        </div>
-    );
+    const [etat, setEtat] = useState(false);
+    const etatfunction = () =>{
+        setEtat(!etat);
+        console.log(etat);
+    }
+
+    if(etat)
+    {
+        return (
+            <div>
+                <Head/>
+                <Onglet/>
+                <Setting/>
+                <button onClick={etatfunction} >Generer le rapport</button>
+                <Body/>
+            </div>
+        );
+    }
+    else
+    {
+        return (
+            <div>
+                <Head/>
+                <Onglet/>
+                <Setting/>
+                <button onClick={etatfunction} >Generer le rapport</button>
+            </div>
+        );
+    }
+    
 };
 
 export default Home;
