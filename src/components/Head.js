d21761fc453465c345561f75fda63c1f70f3a7e6
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-05-17 12:08:30 +0100

    ajout dans header

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import logo from '../images/logo.png';
+import email from '../images/email.png';
+import etoile from '../images/etoile.jpeg';
+import message from '../images/message.png';
+import pays from '../images/pays.jpeg';
+import profile from '../images/profile.png';
+import recherche from '../images/recherche.jpeg';
+import valider from '../images/valider.png';
 
 const HeadStyle={
     height: '60px',
     display: 'flex',
+    background: 'green'
 }
 
 const Head = () => {
     return (
         <div style={HeadStyle}>
-            <h3>head</h3>
+            <img src={valider}/>
+            <img src={message}/>
+            <img src={email}/>
+            <img src={etoile}/>
             <img src={logo} /> 
+            <img src={pays}/>
+            <img src={recherche}/>
+            <img src={profile}/>
         </div>
     );
 };
