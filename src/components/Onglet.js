467ee41aa9aace8506d9c7c2caf546b8d91d6a63
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-05-26 15:06:01 +0100

    ajout des lien dans onglet

--- src/components/Onglet.js
+++ src/components/Onglet.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import rapport from '../images/rapport.jpeg'
+import charts from '../images/charts.png'
+import clients from '../images/clients.png'
+import geomax from '../images/geomax.jpeg'
+import parametrage from '../images/parametrage.png'
+import { Link } from 'react-router-dom';
+import { hover } from '@testing-library/user-event/dist/hover';
+
+const styleT={
+    height: '15px',
+}
+
+const linkStyle = {
+    margin: "1rem",
+    textDecoration: "none",
+    color: 'black',
+};
 
 const Onglet = () => {
     return (
-        <div>
-            <h1>Onglet</h1>
+        <div style={{background:'white',margin:10}}>
+            <nav style={{display:'flex'}}>
+                <div> 
+                    <img src={rapport} style={styleT}/>
+                    <Link to="/home" style={linkStyle}>Rapport</Link>  
+                </div>
+                <div> 
+                    <img src={charts} style={styleT}/>
+                    <Link to="/home" style={linkStyle}>Charts</Link>  
+                </div>
+                <div> 
+                    <img src={clients} style={styleT}/>
+                    <Link to="/home" style={linkStyle}>Clients</Link>              
+                </div>
+                <div>
+                    <img src={geomax} style={styleT}/> 
+                    <Link to="/home" style={linkStyle}>Geomax</Link>               
+                </div>
+                <div>
+                    <img src={parametrage} style={styleT}/> 
+                    <Link to="/home" style={linkStyle}>Parametrage</Link>       
+                </div>
+            </nav>
         </div>
     );
 };
