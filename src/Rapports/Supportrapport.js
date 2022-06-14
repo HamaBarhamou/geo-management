684a4bfec429c077b232c2bac1a284754e248bc1
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-06-14 21:59:16 +0100

    mise en forma dans un tableau: tentative 1

--- src/Rapports/Supportrapport.js
+++ src/Rapports/Supportrapport.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react';
 import Excesvitesse from '../components/Excesvitesse';
 import Security from '../components/Security';
-import Performance from '../components/Performance'
+import Performance from '../components/Performance';
+import Table from 'rc-table';
+
+const columns_Generale = [
+    {
+        title: 'Id',
+        dataIndex: 'id',
+        key: 'id',
+        width: 100,
+    },
+    {
+      title: 'Engins Roulants',
+      dataIndex: 'name',
+      key: 'name',
+      width: 200,
+    },
+    {
+      title: 'Alarmes',
+      dataIndex: 'alarmes',
+      key: 'address',
+      width: 200,
+    },
+  ];
+  
+  /*const data = [
+    { name: 'Jack', id: 28, alarmes: 'some where'},
+    { name: 'Rose', id: 36, alarmes: 'some where'},
+  ];*/
+
+  const data = []
 
 const Supportrapport = (props) => {
     const [date, setDate] = useState(new Date())
@@ -15,13 +44,12 @@ const Supportrapport = (props) => {
     if (props.type == "General")
             return(
                 <div>
-                    <h1>Rapport</h1>
-
-                    <ul>
-                        {props.data.data.map(todo=>
-                            <li>{todo.machineName}</li>
-                        )}
-                    </ul>
+                    <h1>Rapport </h1>
+                    {props.data.data.map(todo=>{
+                        //if(data.indexOf(todo.carId) == -1)
+                            data.push({"name":todo.machineName,"id":todo.carId, alarmes:"todo.alarmCounts.map()"})
+                    })}
+                    <Table columns={columns_Generale} data={data} />
                     <Security/>
                     <Performance/>
                 </div>
